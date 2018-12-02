2b951b032b3ef9e963b02a0ba22477e2706cad1b
Adones Pitogo <pitogo.adones@gmail.com>
2018-12-02 21:23:14 +0800

    initial commit

---
+++ ado-ng-macaddress.js
@@ -0,0 +1,19 @@
+
+(function () {
+'use strict';
+
+  var App = angular.module('ado.macaddress-filter', []);
+
+  App.filter('macaddress', function() {
+    return function(mac) {
+      try {
+        return mac.match(/.{1,2}/g).map(function(c) {
+          return c.toUpperCase();
+        }).join('-');
+      } catch(e) {
+        return mac;
+      }
+    };
+  });
+
+})();
